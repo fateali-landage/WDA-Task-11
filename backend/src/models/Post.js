5d797b935f144c920ca4903e714b50659c8d2075
Fatheali Landage <fatealilandage@gmail.com>
2026-02-01 00:46:14 +0530

    Create Post.js

---
+++ backend/src/models/Post.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Post', PostSchema);
